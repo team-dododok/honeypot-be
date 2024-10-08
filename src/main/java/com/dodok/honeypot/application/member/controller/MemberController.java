918d47b5bd60b8bef85cbd1f71e24258d7aee6ae
OJOJIN <ojy101039@gmail.com>
2024-10-08 22:48:05 +0900

    DD-9 feat 도메인별 컨트롤러 추가

---
+++ src/main/java/com/dodok/honeypot/application/member/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.dodok.honeypot.application.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/member")
+public class MemberController {
+}
---
+++ src/main/java/com/dodok/honeypot/application/stamp/controller/StampController.java
@@ -0,0 +1,11 @@
+package com.dodok.honeypot.application.stamp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/stamp")
+public class StampController {
+}
