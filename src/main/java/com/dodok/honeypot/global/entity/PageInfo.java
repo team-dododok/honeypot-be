c1d88dd13507611921c96ae91633c474390d6275
OJOJIN <ojy101039@gmail.com>
2024-10-08 22:47:41 +0900

    DD-9 feat 페이징관련 객체 추가

---
+++ src/main/java/com/dodok/honeypot/global/entity/PageInfo.java
@@ -0,0 +1,22 @@
+package com.dodok.honeypot.global.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import org.springframework.data.domain.Page;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record PageInfo(
+        int pageNum,
+        int pageSize,
+        long totalElements,
+        long totalPages
+) {
+    public static PageInfo of(Page<?> page) {
+        return PageInfo.builder()
+                .pageNum(page.getNumber())
+                .pageSize(page.getSize())
+                .totalElements(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .build();
+    }
+}
