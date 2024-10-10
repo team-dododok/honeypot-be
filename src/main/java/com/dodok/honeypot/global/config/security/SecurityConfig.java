dadc5a543235164d6b87bff515ce317c930e06eb
OJOJIN <ojy101039@gmail.com>
2024-10-10 23:14:40 +0900

    DD-11 feat 화이트리스트 추가

--- src/main/java/com/dodok/honeypot/global/config/security/SecurityConfig.java
+++ src/main/java/com/dodok/honeypot/global/config/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .formLogin((auth) -> auth.disable())
                 .httpBasic((auth) -> auth.disable())
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/health")
+                        .requestMatchers("/api/health", "/api/member/info")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
