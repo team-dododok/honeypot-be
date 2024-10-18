8fc37fe60e27cbea69408e8596515d3ac8f0d0d7
j2noo <wlsdndml213@gmail.com>
2024-10-18 18:34:36 +0900

    DD-26 feat : 사용자가 획득한 뱃지 정보 조회 컨트롤러 생성

--- src/main/java/com/dodok/honeypot/application/badge/controller/BadgeController.java
+++ src/main/java/com/dodok/honeypot/application/badge/controller/BadgeController.java
@@ -1,11 +1,23 @@
 package com.dodok.honeypot.application.badge.controller;
 
+import com.dodok.honeypot.domain.badge.dto.req.CompletedBadgeReqDto;
+import com.dodok.honeypot.domain.badge.dto.res.CompletedBadgeResDto;
+import com.dodok.honeypot.domain.badge.service.GetBadgeInfoService;
+import com.dodok.honeypot.global.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/badge")
 public class BadgeController {
+    private final GetBadgeInfoService getBadgeInfoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SuccessResponse<?>> getBadgeInfo(@PathVariable("id") CompletedBadgeReqDto req){
+        CompletedBadgeResDto res = getBadgeInfoService.execute(req);
+        return SuccessResponse.ok(res);
+    }
+
 }
