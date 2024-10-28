fc59f0e714fad8e933e4e4e70b7cfff2e5882824
j2noo <wlsdndml213@gmail.com>
2024-10-28 16:49:16 +0900

    DD-39 refactor : QEntity를 static import

--- src/main/java/com/dodok/honeypot/domain/badge/repository/BadgeInfoQueryRepositoryImpl.java
+++ src/main/java/com/dodok/honeypot/domain/badge/repository/BadgeInfoQueryRepositoryImpl.java
@@ -1,13 +1,14 @@
     package com.dodok.honeypot.domain.badge.repository;
 
     import com.dodok.honeypot.domain.badge.dto.CompletedBadgeInfo;
-    import com.dodok.honeypot.domain.badge.entity.QBadge;
-    import com.dodok.honeypot.domain.badge.entity.QBadgeComplete;
     import com.querydsl.core.types.Projections;
     import com.querydsl.jpa.impl.JPAQueryFactory;
     import lombok.RequiredArgsConstructor;
 
     import java.util.List;
+
+    import static com.dodok.honeypot.domain.badge.entity.QBadge.badge;
+    import static com.dodok.honeypot.domain.badge.entity.QBadgeComplete.badgeComplete;
 
 
     @RequiredArgsConstructor
@@ -16,16 +17,13 @@
 
         @Override
         public List<CompletedBadgeInfo> findAllCompletedBadgeByMemberId(Long memberId) {
-            QBadgeComplete qBadgeComplete = QBadgeComplete.badgeComplete;
-            QBadge qBadge = QBadge.badge;
 
-            // TODO : fetch시 예외처리를 해야하는가?
             return queryFactory.select(Projections.constructor(CompletedBadgeInfo.class,
-                            qBadge.id,
-                            qBadgeComplete.createdAt))
-                    .from(qBadgeComplete)
-                    .join(qBadge).on(qBadgeComplete.badge.id.eq(qBadge.id))
-                    .where(qBadgeComplete.member.id.eq(memberId))
+                            badge.id,
+                            badgeComplete.createdAt))
+                    .from(badgeComplete)
+                    .join(badge).on(badgeComplete.badge.id.eq(badge.id))
+                    .where(badgeComplete.member.id.eq(memberId))
                     .fetch();
         }
     }
