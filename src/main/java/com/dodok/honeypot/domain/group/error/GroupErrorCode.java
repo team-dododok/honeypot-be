f15b4cb66f28879ef6ea3aefa0b06d55c0be9a96
OJOJIN <ojy101039@gmail.com>
2024-10-29 23:34:19 +0900

    DD-52 feat : 그룹 엔티티 조회 오류코드 추가

--- src/main/java/com/dodok/honeypot/domain/group/error/GroupErrorCode.java
+++ src/main/java/com/dodok/honeypot/domain/group/error/GroupErrorCode.java
@@ -11,6 +11,12 @@ import org.springframework.http.HttpStatus;
 public enum GroupErrorCode implements ErrorCode {
 
     /**
+     * 404 Not Found
+     */
+    GROUP_ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 그룹을 찾을 수 없습니다."),
+    MEMBER_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자에게서 해당 그룹을 찾을 수 없습니다."),
+
+    /**
      * 409 Conflict
      */
     DUPLICATE_GROUP_NAME(HttpStatus.CONFLICT, "이미 해당 그룹명이 있습니다.");
