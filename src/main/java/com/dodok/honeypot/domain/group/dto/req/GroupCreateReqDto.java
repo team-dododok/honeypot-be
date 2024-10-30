a4858c11c9b3f1b93636420ac95d9f8b1f10d91f
OJOJIN <ojy101039@gmail.com>
2024-10-31 02:29:55 +0900

    DD-50 fix : 그룹명 valid 조건 변경 및 메세지 변경

--- src/main/java/com/dodok/honeypot/domain/group/dto/req/GroupCreateReqDto.java
+++ src/main/java/com/dodok/honeypot/domain/group/dto/req/GroupCreateReqDto.java
@@ -3,7 +3,7 @@ package com.dodok.honeypot.domain.group.dto.req;
 import jakarta.validation.constraints.Size;
 
 public record GroupCreateReqDto (
-        @Size(min = 2, max = 15, message = "그룹명은 최소 2자 ~ 최대 15자 입니다.")
+        @Size(min = 1, max = 15, message = "15자 이내로 입력해 주세요.")
         String groupName
 ){
 }
