85af37f7db183e84015ca32ba7c7ba8716a2b0f8
OJOJIN <ojy101039@gmail.com>
2024-10-31 02:32:25 +0900

    DD-50 feat : 그룹 헬퍼 테스트코드 작성

--- src/test/java/com/dodok/honeypot/domain/group/helper/GroupHelperTest.java
+++ src/test/java/com/dodok/honeypot/domain/group/helper/GroupHelperTest.java
@@ -4,6 +4,7 @@ import com.dodok.honeypot.domain.group.entity.Group;
 import com.dodok.honeypot.domain.group.repository.GroupRepository;
 import com.dodok.honeypot.domain.member.entity.Member;
 import com.dodok.honeypot.global.error.exception.ConflictException;
+import com.dodok.honeypot.global.error.exception.EntityNotFoundException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -63,7 +64,31 @@ public class GroupHelperTest {
 
         // then
         Assertions.assertThat(findGroup).isEqualTo(group);
+    }
 
+    @Test
+    @DisplayName("그룹id로 그룹 찾기")
+    void findGroupByIdOrElseThrow() {
+        // given
 
+        // when
+        when(groupRepository.findById(group.getId())).thenReturn(Optional.ofNullable(group));
+        Group findGroup = groupHelper.findGroupByIdOrElseThrow(group.getId());
+
+        // then
+        Assertions.assertThat(findGroup).isEqualTo(group);
+    }
+
+    @Test
+    @DisplayName("그룹id로 그룹 찾기 예외")
+    void findGroupByIdOrElseThrowException() {
+        // given
+
+        // when
+        when(groupRepository.findById(2L)).thenReturn(Optional.ofNullable(null));
+
+        // then
+        assertThrows(EntityNotFoundException.class,
+                () -> groupHelper.findGroupByIdOrElseThrow(2L));
     }
 }
