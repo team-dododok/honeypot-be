a14457ef88c4145b80186f7fd6a05ee8ad0742b1
OJOJIN <ojy101039@gmail.com>
2024-10-31 02:30:16 +0900

    DD-50 feat : 그룹 업데이트 요청 dto 추가

---
+++ src/main/java/com/dodok/honeypot/domain/group/dto/req/GroupUpdateReqDto.java
@@ -0,0 +1,10 @@
+package com.dodok.honeypot.domain.group.dto.req;
+
+import jakarta.validation.constraints.Size;
+
+public record GroupUpdateReqDto(
+        Long groupId,
+        @Size(min = 1, max = 15, message = "15자 이내로 입력해 주세요.")
+        String groupName
+){
+}
