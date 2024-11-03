a66711518aa478dcac7bb3d06e496957992d7762
emilywin825 <emilywin825@naver.com>
2024-11-03 18:10:11 +0900

    DD-44 feat : feign client사용을 위해 어플리케이션 클래스에 명시

--- src/main/java/com/dodok/honeypot/HoneypotApplication.java
+++ src/main/java/com/dodok/honeypot/HoneypotApplication.java
@@ -2,8 +2,10 @@ package com.dodok.honeypot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HoneypotApplication {
 
 	public static void main(String[] args) {
