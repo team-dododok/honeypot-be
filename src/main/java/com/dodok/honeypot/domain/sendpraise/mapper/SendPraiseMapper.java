1c0d4411dbf3ddc28c69764bcf8d4265808bb687
j2noo <wlsdndml213@gmail.com>
2024-11-06 04:37:56 +0900

    DD-70 feat : 보낼칭찬 생성하는 매퍼클래스 생성

--- src/main/java/com/dodok/honeypot/domain/sendpraise/mapper/SendPraiseMapper.java
+++ src/main/java/com/dodok/honeypot/domain/sendpraise/mapper/SendPraiseMapper.java
@@ -1,8 +1,13 @@
 package com.dodok.honeypot.domain.sendpraise.mapper;
 
+import com.dodok.honeypot.domain.sendpraise.dto.res.SendPraiseResDto;
+import com.dodok.honeypot.domain.sendpraise.entity.SendPraise;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SendPraiseMapper {
-
+    public SendPraiseResDto toSendPraiseReqDto(SendPraise sendPraise) {
+        return SendPraiseResDto.of(sendPraise.getUuid());
+    }
 }
+
