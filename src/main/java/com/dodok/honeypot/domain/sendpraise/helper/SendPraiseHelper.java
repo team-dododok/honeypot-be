2ca9a99a0ec46c42c6872f5f023d00f77cf93eb7
j2noo <wlsdndml213@gmail.com>
2024-11-06 04:38:06 +0900

    DD-70 feat : 보낼칭찬 생성하는 헬퍼클래스 생성

--- src/main/java/com/dodok/honeypot/domain/sendpraise/helper/SendPraiseHelper.java
+++ src/main/java/com/dodok/honeypot/domain/sendpraise/helper/SendPraiseHelper.java
@@ -1,6 +1,10 @@
 package com.dodok.honeypot.domain.sendpraise.helper;
 
+import com.dodok.honeypot.domain.group.entity.Group;
+import com.dodok.honeypot.domain.member.entity.Member;
+import com.dodok.honeypot.domain.sendpraise.entity.SendPraise;
 import com.dodok.honeypot.domain.sendpraise.repository.SendPraiseRepository;
+import com.dodok.honeypot.domain.stamp.entity.HoneyStamp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,4 +16,10 @@ public class SendPraiseHelper {
 
     // TODO : 꿀(칭찬)을 보내는 로직 구현 시, CheckSendPraiseBadgeHelper.updateSendPraiseBadge() 호출할 것.
 
+    public SendPraise createSendPraise(String title, String content, Boolean projectStatus,
+                                       String receiverName, Member sender, Group group, HoneyStamp honeyStamp){
+        return sendPraiseRepository.save(
+                SendPraise.createSendPraise(title, content, projectStatus, receiverName, sender, group, honeyStamp)
+        );
+    }
 }
