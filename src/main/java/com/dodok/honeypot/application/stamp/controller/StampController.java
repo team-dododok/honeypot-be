5362f42adb3d2d44304a7cfeccc5e91f46cccb47
j2noo <wlsdndml213@gmail.com>
2024-11-06 07:00:45 +0900

    DD-75 feat : 특정 그룹이 받은 꿀도장을 전부 조회하는 api 생성

--- src/main/java/com/dodok/honeypot/application/stamp/controller/StampController.java
+++ src/main/java/com/dodok/honeypot/application/stamp/controller/StampController.java
@@ -1,12 +1,15 @@
 package com.dodok.honeypot.application.stamp.controller;
 
 import com.dodok.honeypot.domain.stamp.dto.res.AllStampResDto;
+import com.dodok.honeypot.domain.stamp.dto.res.StampInfoByGroupResDto;
+import com.dodok.honeypot.domain.stamp.service.SendStampByGroupHelperService;
 import com.dodok.honeypot.domain.stamp.service.StampService;
 import com.dodok.honeypot.global.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -15,9 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class StampController {
 
     private final StampService stampService;
+    private final SendStampByGroupHelperService sendStampByGroupHelperService;
+
+    /**
+     * 전체 꿀도장의 종류를 반환하는 api
+     * @return 전체 꿀도장
+     */
     @GetMapping("")
     ResponseEntity<SuccessResponse<?>> getAllStamp() {
         AllStampResDto allStamp = stampService.getAllStamp();
         return SuccessResponse.ok(allStamp);
+    }
+
+    /**
+     * groupId에서 받은 꿀도장의 종류를 반환하는 api
+     * @param groupId 사용자가 찾고자 하는 그룹id
+     * @return 꿀도장의 전체 개수와 종류
+     */
+    @GetMapping("/send")
+    ResponseEntity<SuccessResponse<?>> getSendStampByGroup(@RequestParam(name = "group") Long groupId) {
+        StampInfoByGroupResDto sendStampByGroup = sendStampByGroupHelperService.getSendStampByGroup(groupId);
+        return SuccessResponse.ok(sendStampByGroup);
     }
 }
