e5d7cd6a4f3fc6567c3da8a044dd4b24278bba4f
j2noo <wlsdndml213@gmail.com>
2024-11-05 23:12:07 +0900

    DD-73 feat : 전체 꿀도장 조회 api 컨트롤러 생성

--- src/main/java/com/dodok/honeypot/application/stamp/controller/StampController.java
+++ src/main/java/com/dodok/honeypot/application/stamp/controller/StampController.java
@@ -1,6 +1,11 @@
 package com.dodok.honeypot.application.stamp.controller;
 
+import com.dodok.honeypot.domain.stamp.dto.res.AllStampResDto;
+import com.dodok.honeypot.domain.stamp.service.StampService;
+import com.dodok.honeypot.global.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/stamp")
 public class StampController {
+
+    private final StampService stampService;
+    @GetMapping("")
+    ResponseEntity<SuccessResponse<?>> getAllStamp() {
+        AllStampResDto allStamp = stampService.getAllStamp();
+        return SuccessResponse.ok(allStamp);
+    }
 }
