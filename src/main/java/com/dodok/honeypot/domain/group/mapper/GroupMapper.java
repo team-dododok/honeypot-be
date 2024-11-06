f4b1f9ee005de08eae11735d46b298a9561d8a2c
OJOJIN <ojy101039@gmail.com>
2024-11-07 00:13:59 +0900

    DD-51 feat : CheckGroupNameResDto 변환 메퍼 생성

--- src/main/java/com/dodok/honeypot/domain/group/mapper/GroupMapper.java
+++ src/main/java/com/dodok/honeypot/domain/group/mapper/GroupMapper.java
@@ -1,8 +1,11 @@
 package com.dodok.honeypot.domain.group.mapper;
 
+import com.dodok.honeypot.domain.group.dto.res.CheckGroupNameResDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GroupMapper {
-
+    public CheckGroupNameResDto toCheckGroupNameResDto(Boolean isDuplicate) {
+        return CheckGroupNameResDto.of(isDuplicate);
+    }
 }
