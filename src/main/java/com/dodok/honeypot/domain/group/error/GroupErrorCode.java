0a6dee9db783df971e8bd35e858825b0d22513bc
OJOJIN <ojy101039@gmail.com>
2024-11-08 18:37:19 +0900

    DD-53 feat : 그룹 관련 error code 추가

--- src/main/java/com/dodok/honeypot/domain/group/error/GroupErrorCode.java
+++ src/main/java/com/dodok/honeypot/domain/group/error/GroupErrorCode.java
@@ -11,10 +11,20 @@ import org.springframework.http.HttpStatus;
 public enum GroupErrorCode implements ErrorCode {
 
     /**
+     * 400 Bad Request
+     */
+    NOT_VALID_GROUP_ORDER_IDX(HttpStatus.BAD_REQUEST, "그룹 순서 idx가 유효하지 않습니다. 유효한 값으로 요청해주세요"),
+
+    /**
      * 404 Not Found
      */
     GROUP_ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 그룹을 찾을 수 없습니다."),
     MEMBER_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자에게서 해당 그룹을 찾을 수 없습니다."),
+
+    /**
+     * 403 Forbidden
+     */
+    MEMBER_NOT_GROUP_OWNER_FORBIDDEN(HttpStatus.FORBIDDEN, "해당 그룹들에 대한 사용자의 권한이 없습니다."),
 
     /**
      * 409 Conflict
