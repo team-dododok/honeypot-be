405532e4c8a1ee5e9a25259b157e057f6828aff4
OJOJIN <ojy101039@gmail.com>
2024-11-08 18:37:41 +0900

    DD-53 feat : 그룹 순서 변경 API 추가

--- src/main/java/com/dodok/honeypot/application/group/controller/GroupController.java
+++ src/main/java/com/dodok/honeypot/application/group/controller/GroupController.java
@@ -1,9 +1,11 @@
 package com.dodok.honeypot.application.group.controller;
 
 import com.dodok.honeypot.domain.group.dto.req.GroupCreateReqDto;
+import com.dodok.honeypot.domain.group.dto.req.GroupOrderUpdateReqDto;
 import com.dodok.honeypot.domain.group.dto.req.GroupUpdateReqDto;
 import com.dodok.honeypot.domain.group.service.CreateGroupService;
 import com.dodok.honeypot.domain.group.service.DeleteGroupService;
+import com.dodok.honeypot.domain.group.service.UpdateGroupOrderService;
 import com.dodok.honeypot.domain.group.service.UpdateGroupService;
 import com.dodok.honeypot.global.dto.SuccessResponse;
 import jakarta.validation.Valid;
@@ -19,6 +21,7 @@ public class GroupController {
     private final CreateGroupService createGroupService;
     private final DeleteGroupService deleteGroupService;
     private final UpdateGroupService updateGroupService;
+    private final UpdateGroupOrderService updateGroupOrderService;
 
     @PostMapping
     public ResponseEntity<SuccessResponse<?>> createGroup(@RequestParam(name = "id") final Long memberId,
@@ -34,10 +37,17 @@ public class GroupController {
         return SuccessResponse.ok(null);
     }
 
-    @PatchMapping
+    @PatchMapping("/name")
     public ResponseEntity<SuccessResponse<?>> updateGroup(@RequestParam(name = "id") final Long memberId,
-                                                              @RequestBody final GroupUpdateReqDto requestDto) {
+                                                          @RequestBody final GroupUpdateReqDto requestDto) {
         updateGroupService.execute(memberId, requestDto);
+        return SuccessResponse.ok(null);
+    }
+
+    @PatchMapping("/order")
+    public ResponseEntity<SuccessResponse<?>> updateGroupOrder(@RequestParam(name = "id") final Long memberId,
+                                                               @RequestBody final GroupOrderUpdateReqDto requestDto) {
+        updateGroupOrderService.execute(memberId, requestDto);
         return SuccessResponse.ok(null);
     }
 }
