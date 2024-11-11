e1c89a6e484130aabe5aeed39e26d1ae981c2501
j2noo <wlsdndml213@gmail.com>
2024-11-12 01:14:51 +0900

    DD-79 feat : 보낸 칭찬의 uuid를 통해 칭찬을 저장하는 컨트롤러

--- src/main/java/com/dodok/honeypot/application/receivepraise/controller/ReceivePraiseController.java
+++ src/main/java/com/dodok/honeypot/application/receivepraise/controller/ReceivePraiseController.java
@@ -1,6 +1,12 @@
 package com.dodok.honeypot.application.receivepraise.controller;
 
+import com.dodok.honeypot.domain.receivepraise.dto.req.SaveReceivePraiseReqDto;
+import com.dodok.honeypot.domain.sendpraise.SaveReceivePraiseService;
+import com.dodok.honeypot.global.dto.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/receive-praise")
 public class ReceivePraiseController {
+
+    private final SaveReceivePraiseService saveReceivePraiseService;
+
+    @PostMapping("")
+    public ResponseEntity<SuccessResponse<?>> saveReceivePraise(@RequestBody SaveReceivePraiseReqDto req) {
+        saveReceivePraiseService.execute(req);
+        return SuccessResponse.ok(null);
+    }
 }
