e6858510224fde36d6ac472b0e61adf1918911c3
j2noo <wlsdndml213@gmail.com>
2024-11-22 16:45:39 +0900

    DD-97 feat : 해당 칭찬으로 받은칭찬들 전부 조회하는 레퍼지토리

--- src/main/java/com/dodok/honeypot/domain/receivepraise/repository/ReceivePraiseRepository.java
+++ src/main/java/com/dodok/honeypot/domain/receivepraise/repository/ReceivePraiseRepository.java
@@ -4,7 +4,17 @@ import com.dodok.honeypot.domain.receivepraise.entity.ReceivePraise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReceivePraiseRepository extends JpaRepository<ReceivePraise, Long> {
     Long countByReceiverId(Long receiverId);
+
+    /**
+     * 보낸 칭찬의 id로 전체 받은 칭찬을 조회하는 로직
+     * @param sendPraiseId 보낸칭찬의 id
+     * @return 받은칭찬 리스트
+     *
+     */
+    List<ReceivePraise> findAllBySendPraiseId(Long sendPraiseId);
 }
