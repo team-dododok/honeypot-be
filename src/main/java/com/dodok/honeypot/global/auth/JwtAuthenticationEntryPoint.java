c3ff8a04746994002a3f312d75d06a3afb444404
emilywin825 <emilywin825@naver.com>
2024-11-25 05:30:56 +0900

    DD-109 feat : 인증되지 않은 사용자가 리소스에 접근할 때 처리를 위한 AuthenticationEntryPoint 추가

--- src/main/java/com/dodok/honeypot/global/auth/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/dodok/honeypot/global/auth/JwtAuthenticationEntryPoint.java
@@ -1,17 +1,37 @@
 package com.dodok.honeypot.global.auth;
 
+import com.dodok.honeypot.global.error.code.ErrorCode;
+import com.dodok.honeypot.global.error.code.GlobalErrorCode;
+import com.dodok.honeypot.global.error.entity.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+//AuthenticationEntryPoint 인터페이스는 인증되지 않은 사용자가 인증이 필요한 요청 엔드포인트로 접근하려 할 때, 예외를 핸들링 할 수 있도록 도와준다.
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException)
             throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        log.error("CONNECT URL" + request.getRequestURI());
+        log.error("인증 정보가 없는 사용자입니다.");
+        ErrorCode errorCode = GlobalErrorCode.UNAUTHORIZED;
+
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.of(errorCode)));
     }
 }
