c4e278160a8c4c54801f45a3c9bed280326353cb
emilywin825 <emilywin825@naver.com>
2024-11-25 05:31:17 +0900

    DD-109 feat : 권한이 부족한 사용자 처리를 위한 JwtAccessDeniedHandler 추가

--- src/main/java/com/dodok/honeypot/global/auth/JwtAccessDeniedHandler.java
+++ src/main/java/com/dodok/honeypot/global/auth/JwtAccessDeniedHandler.java
@@ -1,17 +1,39 @@
 package com.dodok.honeypot.global.auth;
 
+import com.dodok.honeypot.global.error.code.ErrorCode;
+import com.dodok.honeypot.global.error.code.GlobalErrorCode;
+import com.dodok.honeypot.global.error.entity.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
             throws IOException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        log.error("CONNECT URL" + request.getRequestURI());
+        log.error("유저의 권한으로는 접근할 수 없습니다.");
+
+        ErrorCode errorCode = GlobalErrorCode.FORBIDDEN;
+
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.of(errorCode)));
+
     }
 }
