96854db83986472c06a3a683058ae7ead528fced
emilywin825 <emilywin825@naver.com>
2024-11-25 05:28:18 +0900

    DD-109 feat : AuthErrorCode 추가

--- src/main/java/com/dodok/honeypot/domain/auth/error/AuthErrorCode.java
+++ src/main/java/com/dodok/honeypot/domain/auth/error/AuthErrorCode.java
@@ -12,10 +12,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum AuthErrorCode implements ErrorCode {
 
+
+    /**
+     * 400 BADREQUEST
+     */
+    TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST,"헤더에 AccessToken이 존재하지 않습니다."),
+
     /**
      * 401 UNAUTHORIZED
      */
     REFRESH_TOKEN_NOT_MATCH(HttpStatus.UNAUTHORIZED,"해당 유저 ID와 일치하지 않는 REFRESH TOKEN 입니다."),
+    TOKEN_TYPE_NOT_MATCH(HttpStatus.UNAUTHORIZED,"토큰 타입이 일치하지 않습니다."),
+    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED,"Access Token이 만료되었습니다. 재발급 받아주세요."),
+    ACCESS_TOKEN_INVALID(HttpStatus.UNAUTHORIZED,"유효하지 않은 AccessToken입니다."),
+    JWT_TOKEN_MALFORMED(HttpStatus.UNAUTHORIZED,"JWT 토큰의 형식이 올바르지 않습니다."),
+    JWT_TOKEN_UNSUPPORTED(HttpStatus.UNAUTHORIZED,"지원하지 않는 JWT 토큰입니다."),
+    JWT_TOKEN_MISSING(HttpStatus.UNAUTHORIZED,"JWT 토큰이 없거나 비어 있습니다."),
 
     /**
      * 404 Not Found
