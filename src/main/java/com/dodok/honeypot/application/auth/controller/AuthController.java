e02437a8e8b6c0048096a1ae813c202b80b63563
j2noo <wlsdndml213@gmail.com>
2024-11-25 12:25:44 +0900

    DD-54 chore : spring mail 의존성 추가

--- src/main/java/com/dodok/honeypot/application/auth/controller/AuthController.java
+++ src/main/java/com/dodok/honeypot/application/auth/controller/AuthController.java
@@ -6,15 +6,19 @@ import com.dodok.honeypot.domain.auth.dto.res.KakaoLoginResDto;
 import com.dodok.honeypot.domain.auth.dto.res.ReissueJwtTokenResDto;
 import com.dodok.honeypot.domain.auth.service.KakaoSocialLoginService;
 import com.dodok.honeypot.global.dto.SuccessResponse;
+import com.dodok.honeypot.global.utils.MailUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     private final KakaoSocialLoginService kakaoSocialLoginService;
+    private final MailUtils mailUtils;
 
     @PostMapping("/kakao-login") //로그인
     public ResponseEntity<SuccessResponse<?>> kakaoLogin(
@@ -32,8 +36,31 @@ public class AuthController {
     }
 
     @PostMapping("/reissue")//accessToken 재발행
-    public ResponseEntity<SuccessResponse<?>> reissue(@RequestBody ReissueJwtTokenReqDto reissueJwtTokenDto){
+    public ResponseEntity<SuccessResponse<?>> reissue(@RequestBody ReissueJwtTokenReqDto reissueJwtTokenDto) {
         ReissueJwtTokenResDto resDto = kakaoSocialLoginService.reissue(reissueJwtTokenDto);
         return SuccessResponse.ok(resDto);
+    }
+
+    @PostMapping("/mail")
+    public ResponseEntity<SuccessResponse<?>> sendMail(@RequestBody String mail) {
+//        mailUtils.sendMail(mail);
+        HashMap<String, Object> map = new HashMap<>();
+
+        int number;
+
+        try {
+            number = mailUtils.sendMail(mail);
+            String num = String.valueOf(number);
+
+            map.put("success", Boolean.TRUE);
+            map.put("number", num);
+        } catch (Exception e) {
+            map.put("success", Boolean.FALSE);
+            map.put("error", e.getMessage());
+        }
+
+        System.out.println("map = " + map);
+        return null;
+
     }
 }
