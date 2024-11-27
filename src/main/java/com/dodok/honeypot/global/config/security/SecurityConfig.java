c80f8f9b18822a0bb0f65067b51955e3513805d3
j2noo <wlsdndml213@gmail.com>
2024-11-27 21:49:29 +0900

    DD-126 feat : 카카오톡 공유하기 성공시 콜백 api와 폴링 api 컨트롤러 부분 만 구현

--- src/main/java/com/dodok/honeypot/global/config/security/SecurityConfig.java
+++ src/main/java/com/dodok/honeypot/global/config/security/SecurityConfig.java
@@ -26,7 +26,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/health", "/api/member/info/*", "/api/member/search", "/api/member","/api/member/*",
                                 "/api/badge",
                                 "/api/group", "/api/group/*", "/api/group/order", "/api/group/name",
-                                "api/stamp", "api/send-praise", "api/stamp/*", "api/receive-praise", "api/receive-praise/group", "api/send-praise/group",
+                                "api/stamp", "api/send-praise", "api/stamp/*", "api/receive-praise", "api/receive-praise/group",
+                                "api/send-praise/group","api/send-praise/check",
                                 "/api/auth/*")
                         .permitAll()
                         .anyRequest()
