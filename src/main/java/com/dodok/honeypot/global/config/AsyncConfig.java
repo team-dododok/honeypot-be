f378557a20ca7baf30b114ff2e8c8baa504f9fea
j2noo <wlsdndml213@gmail.com>
2024-11-27 19:18:12 +0900

    DD-54 style : 불필요한 어노테이션 삭제

--- src/main/java/com/dodok/honeypot/global/config/AsyncConfig.java
+++ src/main/java/com/dodok/honeypot/global/config/AsyncConfig.java
@@ -4,13 +4,11 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
-@EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
 
     @Override
