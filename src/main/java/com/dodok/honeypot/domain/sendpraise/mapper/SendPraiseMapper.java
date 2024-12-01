b1427f06c2431fd3051cdb4a0366bf26069e77cf
OJOJIN <ojy101039@gmail.com>
2024-12-01 16:22:52 +0900

    DD-144 fix : 그룹 내 보낸 칭찬 조회 시 프로필 이미지 함께 반환하도록 수정

--- src/main/java/com/dodok/honeypot/domain/sendpraise/mapper/SendPraiseMapper.java
+++ src/main/java/com/dodok/honeypot/domain/sendpraise/mapper/SendPraiseMapper.java
@@ -1,13 +1,16 @@
 package com.dodok.honeypot.domain.sendpraise.mapper;
 
+import com.dodok.honeypot.domain.member.entity.ProfileImage;
 import com.dodok.honeypot.domain.sendpraise.dto.SendPraiseInfo;
 import com.dodok.honeypot.domain.sendpraise.dto.res.GetGroupSendPraiseResDto;
+import com.dodok.honeypot.domain.sendpraise.dto.res.SendPraiseInfoResDto;
 import com.dodok.honeypot.domain.sendpraise.dto.res.SendPraiseResDto;
 import com.dodok.honeypot.domain.sendpraise.entity.SendPraise;
 import com.dodok.honeypot.global.dto.PageInfo;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class SendPraiseMapper {
@@ -15,8 +18,18 @@ public class SendPraiseMapper {
         return SendPraiseResDto.of(sendPraise.getUuid());
     }
 
-    public GetGroupSendPraiseResDto toGetGroupReceivePraiseResDto(List<SendPraiseInfo> sendPraiseInfos, PageInfo pageInfo) {
-        return GetGroupSendPraiseResDto.of(sendPraiseInfos, pageInfo);
+    public GetGroupSendPraiseResDto toGetGroupReceivePraiseResDto(List<SendPraiseInfo> sendPraiseInfos, List<ProfileImage> profileImageUrlList, PageInfo pageInfo) {
+        List<SendPraiseInfoResDto> sendPraiseInfoResDtoList = sendPraiseInfos.stream()
+                .map(sendPraiseInfo -> {
+                    SendPraiseInfoResDto sendPraiseInfoResDto = SendPraiseInfoResDto.of(sendPraiseInfo, profileImageUrlList.get(getImageIdx(profileImageUrlList, sendPraiseInfo)).getImageUrl());
+                    return sendPraiseInfoResDto;
+                })
+                .collect(Collectors.toList());
+        return GetGroupSendPraiseResDto.of(sendPraiseInfoResDtoList, pageInfo);
+    }
+
+    private int getImageIdx(List<ProfileImage> profileImageUrlList, SendPraiseInfo sendPraiseInfo) {
+        return Long.valueOf(sendPraiseInfo.sendPraiseId() % profileImageUrlList.size()).intValue();
     }
 }
 
