373d9a9e9e3c09ce54a3f69ce3ed14d6cf6181cb
j2noo <wlsdndml213@gmail.com>
2024-12-03 15:34:47 +0900

    DD-157 fix : sendPraise의 상태를 업데이트하는 로직에 트랜잭션어노테이션 추가

--- src/main/java/com/dodok/honeypot/domain/sendpraise/service/CheckSendPraiseSentService.java
+++ src/main/java/com/dodok/honeypot/domain/sendpraise/service/CheckSendPraiseSentService.java
@@ -7,16 +7,19 @@ import com.dodok.honeypot.domain.sendpraise.helper.SendPraiseHelper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CheckSendPraiseSentService {
     private final SendPraiseHelper sendPraiseHelper;
     public void execute(CheckSendPraiseSentReqDto req) {
         SendPraise sendPraise = sendPraiseHelper.findByUuidOrElseThrow(req.praiseUuid());
         sendPraise.updateSendStatus(SendStatus.SUCCESS);
-        log.info("praise Uuid : "+req.praiseUuid() + "is Successfully Sent");
-        log.info("requestBody : " + req);
+        log.info("[kakao-talk callback api] requestBody praiseUuid: " + req.praiseUuid());
+        log.info("[kakao-talk callback api] requestBody CHAT_TYPE: " + req.CHAT_TYPE());
+        log.info("[kakao-talk callback api] requestBody HASH_CHAT_ID: " + req.HASH_CHAT_ID());
     }
 }
--- src/main/java/com/dodok/honeypot/domain/sendpraise/service/GetSendPraiseSentService.java
+++ src/main/java/com/dodok/honeypot/domain/sendpraise/service/GetSendPraiseSentService.java
@@ -2,20 +2,24 @@ package com.dodok.honeypot.domain.sendpraise.service;
 
 import com.dodok.honeypot.domain.sendpraise.dto.res.GetSendPraiseSentResDto;
 import com.dodok.honeypot.domain.sendpraise.entity.SendPraise;
+import com.dodok.honeypot.domain.sendpraise.entity.SendStatus;
 import com.dodok.honeypot.domain.sendpraise.helper.SendPraiseHelper;
 import com.dodok.honeypot.domain.sendpraise.mapper.SendPraiseMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class GetSendPraiseSentService {
     private final SendPraiseHelper sendPraiseHelper;
     private final SendPraiseMapper sendPraiseMapper;
     public GetSendPraiseSentResDto execute(String praiseUuid) {
         SendPraise sendPraise = sendPraiseHelper.findByUuidOrElseThrow(praiseUuid);
+        sendPraise.updateSendStatus(SendStatus.SUCCESS);
         return sendPraiseMapper.toGetSendPraiseSentResDto(sendPraise);
     }
 }
