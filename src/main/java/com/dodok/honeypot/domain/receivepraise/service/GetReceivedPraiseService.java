e78ff5976cd151c9adb874d2c1940041d3921935
j2noo <wlsdndml213@gmail.com>
2024-12-03 18:11:42 +0900

    DD-157 refactor : 받은칭찬 조회시, 내가보낸경우 groupId = -2가 아니라 SendStatus.MYSELF로 반환

--- src/main/java/com/dodok/honeypot/domain/receivepraise/service/GetReceivedPraiseService.java
+++ src/main/java/com/dodok/honeypot/domain/receivepraise/service/GetReceivedPraiseService.java
@@ -7,11 +7,13 @@ import com.dodok.honeypot.domain.receivepraise.entity.ReceivePraise;
 import com.dodok.honeypot.domain.receivepraise.helper.ReceivePraiseHelper;
 import com.dodok.honeypot.domain.receivepraise.mapper.ReceivePraiseMapper;
 import com.dodok.honeypot.domain.sendpraise.entity.SendPraise;
+import com.dodok.honeypot.domain.sendpraise.entity.SendStatus;
 import com.dodok.honeypot.domain.sendpraise.helper.SendPraiseHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -20,19 +22,23 @@ import java.util.Optional;
 @Service
 @Transactional(readOnly = true)
 public class GetReceivedPraiseService {
+    private final List<SendStatus> sendStatuses = Arrays.asList(SendStatus.DIRECT, SendStatus.GROUP, SendStatus.MYSELF);
+
     private final SendPraiseHelper sendPraiseHelper;
     private final MemberHelper memberHelper;
     private final ReceivePraiseHelper receivePraiseHelper;
+
     public GetReceivedPraiseResDto execute(String praiseUuid, Optional<Long> memberId) {
-        SendPraise sendPraise = sendPraiseHelper.findByUuidOrElseThrow(praiseUuid);
+        SendPraise sendPraise = sendPraiseHelper.findByUuidAndSendStatusesOrElseThrow(praiseUuid, sendStatuses);
 
         Long savedGroupId = -1L; // 내가 칭찬을 이미 받은 경우, 칭찬이 저장된 그룹의 id
+        SendStatus sendStatus = sendPraise.getSendStatus();
 
         if (memberId.isPresent()) {
-            // 내가 보낸 칭찬인지 확인
+            // 내가 보낸 칭찬인 경우
             if (Objects.equals(sendPraise.getSender().getId(), memberId.get())) {
-                savedGroupId = -2L;
-                return ReceivePraiseMapper.getReceivedPraiseResDto(sendPraise, savedGroupId);
+                sendStatus = SendStatus.MYSELF;
+                return ReceivePraiseMapper.getReceivedPraiseResDto(sendPraise, sendStatus, savedGroupId);
             }
 
             Member receiver = memberHelper.findMemberByIdOrElseThrow(memberId.get());
@@ -47,6 +53,6 @@ public class GetReceivedPraiseService {
             }
         }
 
-        return ReceivePraiseMapper.getReceivedPraiseResDto(sendPraise, savedGroupId);
+        return ReceivePraiseMapper.getReceivedPraiseResDto(sendPraise, sendStatus, savedGroupId);
     }
 }
