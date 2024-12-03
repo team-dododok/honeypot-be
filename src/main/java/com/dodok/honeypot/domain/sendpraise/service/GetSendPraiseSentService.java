df709326998fb8fb47c9064b7b0cdf3c93881a4c
j2noo <wlsdndml213@gmail.com>
2024-12-03 18:10:23 +0900

    DD-157 fix : 잘못된 업데이트로직 제거

--- src/main/java/com/dodok/honeypot/domain/sendpraise/service/GetSendPraiseSentService.java
+++ src/main/java/com/dodok/honeypot/domain/sendpraise/service/GetSendPraiseSentService.java
@@ -2,7 +2,6 @@ package com.dodok.honeypot.domain.sendpraise.service;
 
 import com.dodok.honeypot.domain.sendpraise.dto.res.GetSendPraiseSentResDto;
 import com.dodok.honeypot.domain.sendpraise.entity.SendPraise;
-import com.dodok.honeypot.domain.sendpraise.entity.SendStatus;
 import com.dodok.honeypot.domain.sendpraise.helper.SendPraiseHelper;
 import com.dodok.honeypot.domain.sendpraise.mapper.SendPraiseMapper;
 import lombok.RequiredArgsConstructor;
@@ -12,14 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class GetSendPraiseSentService {
     private final SendPraiseHelper sendPraiseHelper;
     private final SendPraiseMapper sendPraiseMapper;
     public GetSendPraiseSentResDto execute(String praiseUuid) {
         SendPraise sendPraise = sendPraiseHelper.findByUuidOrElseThrow(praiseUuid);
-        sendPraise.updateSendStatus(SendStatus.SUCCESS);
         return sendPraiseMapper.toGetSendPraiseSentResDto(sendPraise);
     }
 }
